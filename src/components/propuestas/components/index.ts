975163937dc3bb67a2d8979e2ad052a8149fbee2
Fabricio Spiritosi <fspiritosi@gmail.com>
2025-11-18 07:02:54 -0300

    fix: correct component and type names in propuestas and servicios module exports
    - Renamed propuestas exports from Cliente* to Propuesta* to match actual component names
    - Updated propuestas action exports from createCliente/updateCliente/deleteCliente to createPropuesta/updatePropuesta/deletePropuesta
    - Renamed servicios exports from Cliente* to Service*/Servicio* for consistency
    - Updated servicios action exports from getClientes/createCliente to getServicios/createServicio
    - Fixed type exports

--- src/components/propuestas/components/index.ts
+++ src/components/propuestas/components/index.ts
@@ -3,6 +3,6 @@ export { PropuestasTable } from "./propuestas-table";
 export { PropuestasTableWrapper } from "./propuestas-table-wrapper";
 export { AddPropuestaButton } from "./add-propuesta-button";
 export { PropuestaForm } from "./propuesta-form";
-export { ClienteRowActions } from "./propuesta-row-actions";
+export { PropuestaRowActions } from "./propuesta-row-actions";
 export { getPropuestas, type PropuestaTecnica } from "./actions";
-export { createCliente, updateCliente, deleteCliente } from "./cliente-actions";
+export { createPropuesta, updatePropuesta, deletePropuesta } from "./propuesta-actions";
